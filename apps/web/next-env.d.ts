0a2354d4240a90cd2a3c0450508a207183dbc02a
Андрей Щегляев <126947513+andreyvbvbvb@users.noreply.github.com>
2026-02-03 20:19:55 +0200

    fix: include r3f intrinsic typings

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference types="@react-three/fiber" />
+/// <reference path="./types/react-three-fiber.d.ts" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
