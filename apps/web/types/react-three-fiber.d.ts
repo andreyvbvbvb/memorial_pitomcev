f264f77797a89e1a398a9eea456ae0a031a4d6e4
Андрей Щегляев <126947513+andreyvbvbvb@users.noreply.github.com>
2026-02-03 20:14:38 +0200

    fix: add group intrinsic element

--- apps/web/types/react-three-fiber.d.ts
+++ apps/web/types/react-three-fiber.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       primitive: any;
+      group: any;
       color: any;
       ambientLight: any;
       directionalLight: any;
